7c8dac7d58a16ea7fd196d762b525553089559c3
Julien Schueller <schueller@phimeca.com>
2017-08-19 08:16:51 +0200

    Fix boost.python detection

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,8 +2,13 @@
 
 export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include"
 
+if test "${PY3K}" = "1"
+then
+  with_boost_python3="--with-boost-python=boost_python3"
+fi
+
 ./autogen.sh
-./configure --prefix=${PREFIX} --enable-python --enable-onlylib --with-eigen3-include=${PREFIX}/include/eigen3
+./configure --prefix=${PREFIX} --enable-python --enable-onlylib --with-eigen3-include=${PREFIX}/include/eigen3 ${with_boost_python3}
 make -j${CPU_COUNT}
 make install
 mv ${PREFIX}/lib/lcmaes.so ${SP_DIR}
