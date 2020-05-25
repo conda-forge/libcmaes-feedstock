f79ebafbbdfc80d61ff5f70f04fc2a88b6ca3a9f
Andreas Krämer <kraemer.research@gmail.com>
2020-05-25 13:29:38 -0400

    rewrite patch

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -11,6 +11,6 @@ if errorlevel 1 exit 1
 cmake --build . --config Release --target install
 if errorlevel 1 exit 1
 
-ctest -C Release --output-on-failure --timeout 100
+ctest -C Release --output-on-failure 
 if errorlevel 1 exit 1
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
-if [ "$(uname)" = "Darwin" ]; then 
-    export NO_LINK_PYTHON="-DLINK_PYTHON=OFF";
-fi
-
 cmake \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DPYTHON_EXECUTABLE=${PREFIX}/bin/python \
     -DCMAKE_BUILD_TYPE=Release \
     -DUSE_COMPILE_FEATURES=OFF \
     -DPYTHON_SITE_PACKAGES=${SP_DIR} \
-    ${NO_LINK_PYTHON} \
     $SRC_DIR
 
 make
