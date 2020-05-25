e4f99daefc681655b0e40062a1d2e4fcace50656
Andreas Krämer <kraemer.research@gmail.com>
2020-05-25 06:31:07 -0400

    boost version pinned

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,13 +3,14 @@
 PYTHON_INCLUDE_DIR=`python -c "from sysconfig import get_paths; info = get_paths(); print(info['include'])"`
 export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include -I${PYTHON_INCLUDE_DIR}"
 
-#if test "${PY3K}" = "1"
-#then
-#  with_boost_python3="--with-boost-python=boost_python3"
-#fi
+if test "${PY3K}" = "1"
+then
+  with_boost_python3="--with-boost-python=boost_python3"
+fi
 
 ./autogen.sh
-./configure --prefix=${PREFIX} --with-boost-libdir=${PREFIX}/lib --enable-onlylib --with-eigen3-include=${PREFIX}/include/eigen3 #${with_boost_python3} --enable-python 
+./configure --prefix=${PREFIX} --with-boost-libdir=${PREFIX}/lib --enable-onlylib --with-eigen3-include=${PREFIX}/include/eigen3 ${with_boost_python3} --enable-python 
+
 make -j${CPU_COUNT}
 make install
 mv ${PREFIX}/lib/lcmaes.so ${SP_DIR}
