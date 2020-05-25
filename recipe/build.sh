ddaeaad138eb4282089af39895d2f0395900531b
Andreas Krämer <kraemer.research@gmail.com>
2020-05-25 08:08:58 -0400

    No python linking on macos

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ "$(uname)" != "Darwin" ]; then 
+    export NO_LINK_PYTHON="-DLINK_PYTHON=OFF"
+    export CMAKE_CXX_FLAGS="-undefined dynamic_lookup"
+fi
 
 cmake \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
@@ -7,6 +11,9 @@ cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DUSE_COMPILE_FEATURES=OFF \
     -DPYTHON_SITE_PACKAGES=${SP_DIR} \
+    ${NO_LINK_PYTHON} \
+    -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS} \
     $SRC_DIR
+
 make
 make install
